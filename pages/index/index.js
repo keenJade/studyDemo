291a4628ea3e5f389aebc6b15d3d86340126c274
keenjade <972782465@qq.com>
2019-11-28 16:24:28 +0800

    indexPage+goodsListTemplate+pastCommStyle

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,13 +1,49 @@
 //index.js
 //获取应用实例
-
 Page({
   data: {
-  
+    background: ['green', 'yellow', 'blue'],
+    indicatorDots: true,
+    vertical: false,
+    autoplay: true,
+    interval: 2000,
+    duration: 500,
+    tabs: [{
+        "time": "26日",
+        "text": "往期精选"
+      },
+      {
+        "time": "10:00",
+        "text": "预热中"
+      }, {
+        "time": "12:00",
+        "text": "预热中"
+      }, {
+        "time": "14:00",
+        "text": "预热中"
+      }, {
+        "time": "16:00",
+        "text": "预热中"
+      }
+    ],
+    activeIndex: 0,
+    sliderOffset: 0,
+    sliderLeft: 0,
+    quiteBuy:["1","2"],
+    isHave:false, // 无货
+    goodsInfo: ["1", "2", "3", "4", "5"] //列表模板数据
   },
-  
-  onLoad: function () {
-    
+
+  onLoad: function() {
+    var that = this;
+   
+  },
+  // 點擊切換
+  tabClick: function(e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   }
- 
+
 })
